874a13bbf8045415092e70d1d1438de36c7fb7cf
Anoop Sarkar <anoop+github@cs.sfu.ca>
2019-09-30 21:49:19 -0700

    added command line usage for zhsegment_check.py

--- zhsegment/zhsegment_check.py
+++ zhsegment/zhsegment_check.py
@@ -17,3 +17,20 @@ def fscore(ref_data, output_data):
     recall = tp / (tp + fn)
     return (2 * ((precision * recall) / (precision + recall)))
 
+if __name__ == '__main__':
+    import os, optparse
+    optparser = optparse.OptionParser()
+    optparser.add_option("-t", "--refcases", dest="ref", default=os.path.join('data', 'reference', 'dev.out'), help="references [default: data/reference/dev.out]")
+    optparser.add_option("-o", "--outputfile", dest="output", default='output.txt', help="output file created by zhsegment.py [default: output.txt]")
+    (opts, _) = optparser.parse_args()
+
+    with open(opts.ref, 'rb') as refh:
+        ref_data = [str(x).strip() for x in refh.read().splitlines()]
+    with open(opts.output, 'rb') as outh:
+        out_data = [str(x).strip() for x in outh.read().splitlines()]
+        output_data = out_data[:len(ref_data)]
+        if len(ref_data) == len(output_data):
+            print("Fscore=", fscore(ref_data, output_data))
+        else:
+            raise ValueError("reference and output are different lengths")
+
