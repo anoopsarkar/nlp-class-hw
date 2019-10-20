c6c7e6998ee75e85db6cf0154aca30a36bf085cf
Anoop Sarkar <anoop+github@cs.sfu.ca>
2019-10-20 14:18:48 -0700

    fixed help message

--- lexsub/default.py
+++ lexsub/default.py
@@ -14,7 +14,7 @@ class LexSub:
 
 if __name__ == '__main__':
     optparser = optparse.OptionParser()
-    optparser.add_option("-i", "--inputfile", dest="input", default=os.path.join('data', 'input', 'dev.txt'), help="file to segment")
+    optparser.add_option("-i", "--inputfile", dest="input", default=os.path.join('data', 'input', 'dev.txt'), help="input file with target word in context")
     optparser.add_option("-w", "--wordvecfile", dest="wordvecfile", default=os.path.join('data', 'glove.6B.100d.magnitude'), help="word vectors file")
     optparser.add_option("-n", "--topn", dest="topn", default=10, help="produce these many guesses")
     optparser.add_option("-l", "--logfile", dest="logfile", default=None, help="log file for debugging")
