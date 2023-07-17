1627ceb8d200daea5cf0c58f9416ccc90b1a9546
Anoop Sarkar <193767+anoopsarkar@users.noreply.github.com>
2023-07-17 10:28:22 -0700

    clean up whitespace

--- signvae/default.py
+++ signvae/default.py
@@ -13,7 +13,7 @@ class VAE(nn.Module):
         - one dropout layer
         - a stack of convolutional layers (we recommend starting
           with 3 of them) interleaved with max pooling layers
-        - a dense layer to project the output from the final 
+        - a dense layer to project the output from the final
           convolution down to size self.z_dim
         - a dense layer to project the encoder output onto mu
         - a dense layer to project the encoder output onto sigma
@@ -32,7 +32,7 @@ class VAE(nn.Module):
     def encode(self,x):
         """
         Given a sequence of images, applies a stack of convolutional layers to
-        each image, flattens the convolved outputs, and projects each one to 
+        each image, flattens the convolved outputs, and projects each one to
         size self.z_dim.
 
         Input:
@@ -46,7 +46,7 @@ class VAE(nn.Module):
 
     def project(self, x):
         """
-        Given an intermediate sequence of encoded images, applies two 
+        Given an intermediate sequence of encoded images, applies two
         projections to each encoding to produce vectors mu and sigma.
 
         Input:
@@ -60,7 +60,7 @@ class VAE(nn.Module):
 
     def reparametrize(self, mu, sigma):
         """
-        Applies the reparametrization trick from 
+        Applies the reparametrization trick from
         https://arxiv.org/pdf/1312.6114v10.pdf
 
         Input:
@@ -70,18 +70,18 @@ class VAE(nn.Module):
                     by self.project()
 
         Returns:
-        - A sequence of codes z of shape (seq_length, self.z_dim) obtained by 
+        - A sequence of codes z of shape (seq_length, self.z_dim) obtained by
           sampling from a normal distribution parameterized by mu and sigma
         """
         return mu + sigma # TODO Your code goes here.
-        
+
     def decode(self, z):
         """
         Given a sequence of variational codes, applies a stack of deconvolutional
         layers (AKA transposed convolutional layers) to recover a sequence of images.
 
         Input:
-        - z:    torch.Tensor of shape (seq_length, self.z_dim) returned by 
+        - z:    torch.Tensor of shape (seq_length, self.z_dim) returned by
                 self.reparametrize()
 
         Returns:
@@ -98,7 +98,7 @@ def kld(mu, log_var):
     - log_var:  log variance of encoder outputs
     - mu:       mean of encoder outputs
 
-    Returns:    D_{KL}(\mathcal{N}(mu, sigma) || \mathcal{N}(0, 1)) 
+    Returns:    D_{KL}(\mathcal{N}(mu, sigma) || \mathcal{N}(0, 1))
                 = log(1 / sigma) + (sigma^2 + mu^2)/2 - 1/2
                 = -0.5*(1 + log(sigma^2) - sigma^2 - mu^2)
     """
@@ -109,23 +109,23 @@ def vae_loss(gen_images, input_images, mu_sigmas):
     Computes BCE reconstruction loss and KL div. loss for VAE outputs.
 
     Input:
-    - gen_images:   list of decoded image sequences, each of shape (seq_length, 
-                    n_channels, img_width, img_height) which defaults to 
+    - gen_images:   list of decoded image sequences, each of shape (seq_length,
+                    n_channels, img_width, img_height) which defaults to
                     (50, 1, 64, 64). In the baseline model, this will contain
-                    one sequence decoded from the VAE itself, and another decoded 
+                    one sequence decoded from the VAE itself, and another decoded
                     from the top layer of the Transformer.
-    - input_images: list of target image sequences, each of shape (seq_length, 
-                    n_channels, img_width, img_height) which defaults to 
+    - input_images: list of target image sequences, each of shape (seq_length,
+                    n_channels, img_width, img_height) which defaults to
                     (50, 1, 64, 64). The nth sequence in gen_images will be
                     evaluated against the nth sequence in input_images to
                     compute the reconstruction loss.
-    - mu_sigmas:    list of (mu, sigma) tuples, where each mu and sigma is a 
+    - mu_sigmas:    list of (mu, sigma) tuples, where each mu and sigma is a
                     sequence of shape (seq_length, VAE.z_dim). In the baseline
                     model, this will contain one tuple from the VAE and another
                     from the Transformer.
 
     Returns:
-    - BCEs: a list containing the total BCE reconstruction loss for each image 
+    - BCEs: a list containing the total BCE reconstruction loss for each image
             sequence
     - KLDs: a list containing the total KL divergence loss for each mu/sigma pair
     """
