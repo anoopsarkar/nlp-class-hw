1a1f1858c45ed8d17fb39e000e7b9fa8c8589d23
Anoop Sarkar <193767+anoopsarkar@users.noreply.github.com>
2023-10-29 13:45:50 -0700

    update default solution and notebook to use pytorch 2.1

--- neuralmt/answer/default.py
+++ neuralmt/answer/default.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-# Python version: 3
+# Python version: 3.8+
 #
+# SFU CMPT413/825 Fall 2023, HW4
 # default solution
 # Simon Fraser University
 # Jetic Gū
@@ -14,37 +15,10 @@ from tqdm import tqdm
 
 import torch
 from torch import nn
+from torchtext.vocab import build_vocab_from_iterator
 
-import pandas as pd
-from torchtext import data
-
-#import support.hyperparams as hp
-#import support.datasets as ds
-
-# hyperparameters
-class hp:
-    # vocab
-    pad_idx = 1
-    sos_idx = 2
-
-    # architecture
-    hidden_dim = 256
-    embed_dim = 256
-    n_layers = 2
-    dropout = 0.2
-    batch_size = 32
-    num_epochs = 10
-    lexicon_cap = 25000
-
-    # training
-    max_lr = 1e-4
-    cycle_length = 3000
-
-    # generation
-    max_len = 50
-
-    # system
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+import support.hyperparams as hp
+from support.datasets import loadTestData, nl_export
 
 # ---YOUR ASSIGNMENT---
 # -- Step 1: Baseline ---
@@ -80,15 +54,14 @@ class AttentionModule(nn.Module):
         """
         alpha = self.calcAlpha(decoder_hidden, encoder_out)
         seq, _, dim = encoder_out.shape
-        context = (torch.sum(encoder_out, dim=0) / seq).reshape(1, 1, dim)
+        context = (torch.sum(encoder_out, dim=0)/seq).reshape(1, 1, dim)
         return context, alpha.permute(2, 0, 1)
 
 
 # -- Step 2: Improvements ---
 # Implement UNK replacement, BeamSearch, translation termination criteria here,
 # you can change 'greedyDecoder' and 'translate'.
-def greedyDecoder(decoder, encoder_out, encoder_hidden, maxLen,
-                  eos_index):
+def greedyDecoder(decoder, encoder_out, encoder_hidden, maxLen):
     seq1_len, batch_size, _ = encoder_out.size()
     target_vocab_size = decoder.target_vocab_size
 
@@ -99,24 +72,25 @@ def greedyDecoder(decoder, encoder_out, encoder_hidden, maxLen,
     decoder_hidden = encoder_hidden[-decoder.n_layers:]
     # start token (ugly hack)
     output = torch.autograd.Variable(
-        outputs.data.new(1, batch_size).fill_(eos_index).long())
+        outputs.data.new(1, batch_size).fill_(hp.sos_idx).long())
     for t in range(maxLen):
         output, decoder_hidden, alpha = decoder(
             output, encoder_out, decoder_hidden)
         outputs[t] = output
         alphas[t] = alpha.data
         output = torch.autograd.Variable(output.data.max(dim=2)[1])
-        if int(output.data) == eos_index:
+        if int(output.data) == hp.eos_idx:
             break
     return outputs, alphas.permute(1, 2, 0)
 
 
-def translate(model, test_iter):
+def translate(model, input_dl):
     results = []
-    for i, batch in tqdm(enumerate(test_iter)):
-        output, attention = model(batch.src)
+    for i, batch in tqdm(enumerate(input_dl)):
+        f, e = batch
+        output, attention = model(f)
         output = output.topk(1)[1]
-        output = model.tgt2txt(output[:, 0].data).strip().split('<EOS>')[0]
+        output = model.tgt2txt(output[:, 0].data).strip().split('<eos>')[0]
         results.append(output)
     return results
 
@@ -197,18 +171,19 @@ class Decoder(nn.Module):
 
 
 class Seq2Seq(nn.Module):
-    def __init__(self, fields=None, srcLex=None, tgtLex=None, build=True):
+    def __init__(self, srcLex=None, tgtLex=None, build=True):
         super(Seq2Seq, self).__init__()
         # If we are loading the model, we don't build it here
         if build is True:
             self.params = {
+                'srcLex': srcLex,
+                'tgtLex': tgtLex,
                 'srcLexSize': len(srcLex.vocab),
                 'tgtLexSize': len(tgtLex.vocab),
                 'embed_dim': hp.embed_dim,
                 'hidden_dim': hp.hidden_dim,
                 'n_layers': hp.n_layers,
                 'dropout': hp.dropout,
-                'fields': fields,
                 'maxLen': hp.max_len,
             }
             self.build()
@@ -227,25 +202,23 @@ class Seq2Seq(nn.Module):
             hidden_dim=self.params['hidden_dim'],
             n_layers=self.params['n_layers'],
             dropout=self.params['dropout'])
-        self.fields = self.params['fields']
         self.maxLen = self.params['maxLen']
 
-    def forward(self, source, maxLen=None, eos_index=2):
+    def forward(self, source, maxLen=None):
         """
         This method implements greedy decoding
         param source: batched input indices
         param maxLen: maximum length of generated output
-        param eos_index: <EOS> token's index
         """
         if maxLen is None:
             maxLen = self.maxLen
         encoder_out, encoder_hidden = self.encoder(source)
 
         return greedyDecoder(self.decoder, encoder_out, encoder_hidden,
-                             maxLen, eos_index)
+                             maxLen)
 
     def tgt2txt(self, tgt):
-        return " ".join([self.fields['tgt'].vocab.itos[int(i)] for i in tgt])
+        return " ".join([self.params['tgtLex'].get_itos()[int(i)] for i in tgt])
 
     def save(self, file):
         torch.save((self.params, self.state_dict()), file)
@@ -255,136 +228,14 @@ class Seq2Seq(nn.Module):
         self.build()
         self.load_state_dict(state_dict)
 
-class DataFrameDataset(data.Dataset):
-    """Class for using pandas DataFrames as a datasource"""
-    def __init__(self, examples, fields, filter_pred=None):
-        """
-        Create a dataset from a pandas dataframe of examples and Fields
-        Arguments:
-            examples pd.DataFrame: DataFrame of examples
-            fields {str: Field}: The Fields to use in this tuple. The
-                string is a field name, and the Field is the associated field.
-            filter_pred (callable or None): use only exanples for which
-                filter_pred(example) is true, or use all examples if None.
-                Default is None
-        """
-        fields = dict(fields)
-        self.examples = examples.apply(
-            SeriesExample.fromSeries, args=(fields,), axis=1).tolist()
-        if filter_pred is not None:
-            self.examples = filter(filter_pred, self.examples)
-        self.fields = dict(fields)
-        # Unpack field tuples
-        for n, f in list(self.fields.items()):
-            if isinstance(n, tuple):
-                self.fields.update(zip(n, f))
-                del self.fields[n]
-
-class SeriesExample(data.Example):
-    """Class to convert a pandas Series to an Example"""
-
-    @classmethod
-    def fromSeries(cls, data, fields):
-        return cls.fromdict(data.to_dict(), fields)
-
-    @classmethod
-    def fromdict(cls, data, fields):
-        ex = cls()
-
-        for key, field in fields.items():
-            if key not in data:
-                raise ValueError(
-                    f"Specified key {key} was not found in the input data")
-            if field is not None:
-                setattr(ex, key, field.preprocess(data[key]))
-            else:
-                setattr(ex, key, data[key])
-        return ex
-
-def biload(src_file, tgt_file, linesToLoad=50000, verbose=False):
-    src = open(src_file).read().lower().strip().split("\n")
-    tgt = open(tgt_file).read().lower().strip().split("\n")
-    return list(map(lambda x: (x[0].strip().split(), x[1].strip().split()), zip(src, tgt)))[:linesToLoad]
-
-def bitext2Dataset(src, tgt, srcLex, tgtLex,
-                   linesToLoad=50000, maxLen=hp.max_len):
-    data = biload(src, tgt, linesToLoad=linesToLoad, verbose=False)
-    data = [(f, e) for f, e in data if len(f) <= maxLen and len(e) <= maxLen]
-    data = {'src': [f for f, e in data],
-            'tgt': [e for f, e in data]}
-
-    df = pd.DataFrame(data, columns=["src", "tgt"])
-    dataset = DataFrameDataset(df, [('src', srcLex), ('tgt', tgtLex)])
-    return dataset
-
-def loadData(batch_size, device=0,
-             trainNum=sys.maxsize, testNum=sys.maxsize):
-    def tokenize(x):
-        return x.split()
-
-    srcLex = data.Field()
-    tgtLex = data.Field(init_token="<SOS>", eos_token="<EOS>")
-
-    train = bitext2Dataset('./data/train.tok.de',
-                           './data/train.tok.en', srcLex, tgtLex,
-                           linesToLoad=trainNum)
-    val = bitext2Dataset('./data/val.tok.de',
-                         './data/val.tok.en', srcLex, tgtLex)
-    test = bitext2Dataset('./data/input/dev.txt',
-                          './data/reference/dev.out', srcLex, tgtLex,
-                          linesToLoad=testNum,
-                          maxLen=sys.maxsize)
-
-    srcLex.build_vocab(train.src, max_size=hp.lexicon_cap)
-    tgtLex.build_vocab(train.tgt, max_size=hp.lexicon_cap)
-
-    train_iter, = data.BucketIterator.splits(
-        (train,),
-        batch_size=batch_size,
-        sort_key=lambda x: len(x.src),
-        device=device,
-        repeat=False)
-
-    val_iter, = data.BucketIterator.splits(
-        (val,),
-        batch_size=batch_size,
-        device=device,
-        repeat=False)
-
-    test_iter = data.Iterator(
-        test,
-        batch_size=1,
-        device=device,
-        sort=False,
-        sort_within_batch=False,
-        shuffle=False,
-        repeat=False)
-
-    return train_iter, val_iter, test_iter, srcLex, tgtLex
-
-def loadTestData(srcFile, srcLex, device=0, linesToLoad=sys.maxsize):
-    def tokenize(x):
-        return x.split()
-    test = bitext2Dataset(srcFile,
-                          srcFile, srcLex, srcLex, linesToLoad,
-                          maxLen=sys.maxsize)
-    test_iter = data.Iterator(
-        test,
-        batch_size=1,
-        device=device,
-        sort=False,
-        sort_within_batch=False,
-        shuffle=False,
-        repeat=False)
-    return test_iter
 
 if __name__ == '__main__':
     optparser = optparse.OptionParser()
     optparser.add_option(
-        "-m", "--model", dest="model", default=os.path.join('data', 'seq2seq_E049.pt'), 
+        "-m", "--model", dest="model",
         help="model file")
     optparser.add_option(
-        "-i", "--input", dest="input", default=os.path.join('data', 'input', 'dev.txt'),
+        "-i", "--input", dest="input", default='./data/input/dev.txt',
         help="input file")
     optparser.add_option(
         "-n", "--num", dest="num", default=sys.maxsize, type='int',
@@ -396,7 +247,7 @@ if __name__ == '__main__':
     model.to(hp.device)
     model.eval()
     # loading test dataset
-    test_iter = loadTestData(opts.input, model.fields['src'],
-                                device=hp.device, linesToLoad=opts.num)
-    results = translate(model, test_iter)
-    print("\n".join(results))
+    test_dl = loadTestData(opts.input, model.params['srcLex'],
+                           device=hp.device, linesToLoad=opts.num)
+    results = translate(model, test_dl)
+    nl_export(results, 'output.txt')
